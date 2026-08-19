9434c7d91ca0cef386a10fbf038ec20771bdd7f2
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-19 13:48:34 +0800

    fix: revert all jakarta.* → javax.* (validation + inject + activation) for JDK 8

--- src/main/java/io/github/easy4j/validation/constraintvalidators/FileNotEmptyValidator.java
+++ src/main/java/io/github/easy4j/validation/constraintvalidators/FileNotEmptyValidator.java
@@ -3,7 +3,7 @@ package io.github.easy4j.validation.constraintvalidators;
 import io.github.easy4j.validation.constraints.FileNotEmpty;
 import io.github.easy4j.validation.provider.FileContentCheckStrategy;
 
-import jakarta.inject.Inject;
+import javax.inject.Inject;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.Objects;
